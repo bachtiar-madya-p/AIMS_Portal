740b8ccbcd73f45e7797b24cdaaca82b55f32d93
@bachtiar-madya-p <bachtiar.madya.p@gmail.com>
2023-07-18 20:54:06 +0700

    update DB schema

--- src/main/resources/common/schema.sql
+++ src/main/resources/common/schema.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
                          `uid` varchar(50) NOT NULL,
                          `role` varchar(50) NOT NULL,
-                         `description` text,
+                         `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
                          `createDt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
                          `modifyDt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                          PRIMARY KEY (`uid`)
@@ -38,8 +38,39 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES ('63b64ac2-fafc-4a9b-978e-e04bfeb471b9','SYS-ADMIN','System Administrator','2023-07-18 08:38:02','2023-07-18 08:38:02');
+INSERT INTO `roles` VALUES ('2e3910ad-bd48-472d-8071-30e24f41bf2c','USER_DELETE','Delete user','2023-07-18 11:23:17','2023-07-18 11:23:17'),('4dfdb19a-47d0-4de0-a332-0d7cd2410bdc','USER_VIEW','Retrieve user details','2023-07-18 11:23:17','2023-07-18 11:23:17'),('63b64ac2-fafc-4a9b-978e-e04bfeb471b9','SYS_ADMIN','System Administrator','2023-07-18 08:38:02','2023-07-18 13:52:44'),('7a5e01b6-e26d-487d-b097-8ff8fee70239','USER_UPDATE','Update user information','2023-07-18 11:23:17','2023-07-18 11:23:17'),('7db8f690-3b7d-4173-8586-f26ce6e6a878','USER_CREATE','Create new user','2023-07-18 11:23:17','2023-07-18 11:23:17'),('c29b633f-4b88-49de-b308-fbf237ea79f3','USER_BULK','Bulk upload user','2023-07-18 11:23:17','2023-07-18 11:23:17');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_roles` (
+                              `id` int NOT NULL AUTO_INCREMENT,
+                              `userid` varchar(50) NOT NULL,
+                              `roleId` varchar(50) DEFAULT NULL,
+                              `createDt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                              `modifyDt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                              PRIMARY KEY (`id`),
+                              KEY `userid` (`userid`),
+                              KEY `roleId` (`roleId`),
+                              CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+                              CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `roles` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (1,'742bd1fd-96c4-4bb3-97be-717f08e0982f','63b64ac2-fafc-4a9b-978e-e04bfeb471b9','2023-07-18 13:20:18','2023-07-18 13:20:24'),(3,'742bd1fd-96c4-4bb3-97be-717f08e0982f','7db8f690-3b7d-4173-8586-f26ce6e6a878','2023-07-18 13:20:18','2023-07-18 13:20:24'),(4,'742bd1fd-96c4-4bb3-97be-717f08e0982f','4dfdb19a-47d0-4de0-a332-0d7cd2410bdc','2023-07-18 13:20:18','2023-07-18 13:20:24'),(5,'742bd1fd-96c4-4bb3-97be-717f08e0982f','7a5e01b6-e26d-487d-b097-8ff8fee70239','2023-07-18 13:20:18','2023-07-18 13:20:24'),(6,'742bd1fd-96c4-4bb3-97be-717f08e0982f','2e3910ad-bd48-472d-8071-30e24f41bf2c','2023-07-18 13:20:18','2023-07-18 13:20:24'),(7,'742bd1fd-96c4-4bb3-97be-717f08e0982f','c29b633f-4b88-49de-b308-fbf237ea79f3','2023-07-18 13:20:18','2023-07-18 13:20:24');
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -58,13 +89,10 @@ CREATE TABLE `users` (
                          `gender` varchar(6) NOT NULL,
                          `email` varchar(50) NOT NULL,
                          `mobile` varchar(20) NOT NULL,
-                         `role` varchar(50) NOT NULL,
-                         `active` tinyint DEFAULT '0',
+                         `active` tinyint DEFAULT '1',
                          `createDt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
                          `modifyDt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                         PRIMARY KEY (`userid`),
-                         KEY `role` (`role`),
-                         CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role`) REFERENCES `roles` (`uid`)
+                         PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -74,7 +102,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('742bd1fd-96c4-4bb3-97be-717f08e0982f','System','Administratir','System Administratir','xxx-xxx-xxx-x','Male','sadmin@mail.com','08123456789','63b64ac2-fafc-4a9b-978e-e04bfeb471b9',0,'2023-07-18 08:39:45','2023-07-18 08:39:45');
+INSERT INTO `users` VALUES ('742bd1fd-96c4-4bb3-97be-717f08e0982f','System','Administratir','System Administratir','xxx-xxx-xxx-x','Male','sadmin@mail.com','08123456789',1,'2023-07-18 08:39:45','2023-07-18 11:33:02');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,7 +131,7 @@ CREATE TABLE `users_authentication` (
 
 LOCK TABLES `users_authentication` WRITE;
 /*!40000 ALTER TABLE `users_authentication` DISABLE KEYS */;
-INSERT INTO `users_authentication` VALUES ('742bd1fd-96c4-4bb3-97be-717f08e0982f','39hs8k','2178e48fa3fecdd5c8c9b452f311d9ee1a1c2224270e82761b227ae09f5ad696',0,'2023-07-18 09:46:19','2023-07-18 09:46:19');
+INSERT INTO `users_authentication` VALUES ('742bd1fd-96c4-4bb3-97be-717f08e0982f','39hs8k','364dbd283b910033a38027fa1ab6af5d01a671995760fbaa314896f24a440e1b',0,'2023-07-18 09:46:19','2023-07-18 13:49:25');
 /*!40000 ALTER TABLE `users_authentication` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,4 +148,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-18 16:47:53
+-- Dump completed on 2023-07-18 20:53:05
